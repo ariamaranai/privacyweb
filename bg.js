fc69d8ac06a1ce4c14a296006b5d98f55cdc8bf2
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-12-10 10:42:02 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,20 +1,16 @@
-(async c=> (
-  c.privacy.websites.adMeasurementEnabled.set({value: !1}),
-  c.privacy.websites.fledgeEnabled.set({value: !1}),
-  c.privacy.websites.hyperlinkAuditingEnabled.set({value: !1}),
-  c.privacy.websites.relatedWebsiteSetsEnabled.set({value: !1}),
-  c.privacy.websites.topicsEnabled.set({value: !1}),
-  c.privacy.websites.referrersEnabled.set({value: !1}),
-  c.privacy.websites.thirdPartyCookiesAllowed.set({value: !1}),
-  c.action.onClicked.addListener(async ()=>
-    c.action.setIcon({
-      path: (await c.privacy.websites.referrersEnabled.get({})).value ?
-        (c.privacy.websites.referrersEnabled.set({value: !1}),
-         c.privacy.websites.thirdPartyCookiesAllowed.set({value: !1}),
-         "on.png") :
-        (c.privacy.websites.referrersEnabled.set({value: !0}),
-         c.privacy.websites.thirdPartyCookiesAllowed.set({value: !0}),
-         "off.png")
-    })
-  )
-))(chrome)
+chrome.privacy.websites.adMeasurementEnabled.set({value: !1}),
+chrome.privacy.websites.fledgeEnabled.set({value: !1}),
+chrome.privacy.websites.hyperlinkAuditingEnabled.set({value: !1}),
+chrome.privacy.websites.relatedWebsiteSetsEnabled.set({value: !1}),
+chrome.privacy.websites.topicsEnabled.set({value: !1}),
+chrome.privacy.websites.referrersEnabled.set({value: !1}),
+chrome.privacy.websites.thirdPartyCookiesAllowed.set({value: !1}),
+chrome.action.onClicked.addListener(async ()=> chrome.action.setIcon({
+  path: (await c.privacy.websites.referrersEnabled.get({})).value ?
+    (c.privacy.websites.referrersEnabled.set({value: !1}),
+     c.privacy.websites.thirdPartyCookiesAllowed.set({value: !1}),
+     "on.png") :
+    (c.privacy.websites.referrersEnabled.set({value: !0}),
+     c.privacy.websites.thirdPartyCookiesAllowed.set({value: !0}),
+     "off.png")
+}))
