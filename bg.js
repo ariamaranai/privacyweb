b5c3692b80e9803153dcf89629231f057aeb9f68
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-01-12 10:17:38 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,28 +1,24 @@
 for (let i = 0; i < 7; ++i)
-  chrome.privacy.websites[
-    [
-      "adMeasurementEnabled",
-      "fledgeEnabled",
-      "hyperlinkAuditingEnabled",
-      "relatedWebsiteSetsEnabled",
-      "topicsEnabled",
-      "referrersEnabled",
-      "thirdPartyCookiesAllowed",
-    ][i]
-  ].set({ value: !1 });
-chrome.action.onClicked.addListener(async () =>
-  chrome.action.setIcon({
-    path: (await chrome.privacy.websites.referrersEnabled.get({})).value
-      ? (chrome.privacy.websites.referrersEnabled.set({ value: !1 }),
-         chrome.privacy.websites.thirdPartyCookiesAllowed.set({ value: !1 }),
-         "on.png")
-      : (chrome.privacy.websites.referrersEnabled.set({ value: !0 }),
-         chrome.privacy.websites.thirdPartyCookiesAllowed.set({ value: !0 }),
-         "off.png")
+  chrome.privacy.websites[[
+    "adMeasurementEnabled",
+    "fledgeEnabled",
+    "hyperlinkAuditingEnabled",
+    "relatedWebsiteSetsEnabled",
+    "topicsEnabled",
+    "referrersEnabled",
+    "thirdPartyCookiesAllowed",
+  ][i]].set({ value: !1 });
+chrome.action.onClicked.addListener(() =>
+  chrome.privacy.websites.referrersEnabled.get({}, details  => {
+    let value = !details.value;
+    let arg = { value };
+    chrome.privacy.websites.referrersEnabled.set(arg),
+    chrome.privacy.websites.thirdPartyCookiesAllowed.set(arg),
+    chrome.action.setIcon({ path: value ? "off.png" : "on.png" })
   })
 );
-chrome.management.onEnabled.addListener(async info =>
-  info.id == chrome.runtime.id &&
-  (await chrome.privacy.websites.referrersEnabled.get({})).value &&
-  chrome.action.setIcon({ path: "off.png" })
+chrome.management.onEnabled.addListener(info =>
+  info.id == chrome.runtime.id && chrome.privacy.websites.referrersEnabled.get({}, details =>
+    details.value && chrome.action.setIcon({ path: "off.png" })
+  )
 );
