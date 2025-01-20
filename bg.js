00005e1d967bd811d2f4eb6a6b52e13aac9b9593
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-01-20 14:51:39 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -8,17 +8,18 @@ for (let i = 0; i < 7; ++i)
     "referrersEnabled",
     "thirdPartyCookiesAllowed",
   ][i]].set({ value: !1 });
+
 chrome.action.onClicked.addListener(() =>
-  chrome.privacy.websites.referrersEnabled.get({}, details  => {
+  chrome.privacy.websites.referrersEnabled.get({}, async details  => {
     let value = !details.value;
     let arg = { value };
-    chrome.privacy.websites.referrersEnabled.set(arg),
-    chrome.privacy.websites.thirdPartyCookiesAllowed.set(arg),
-    chrome.action.setIcon({ path: value ? "off.png" : "on.png" })
+    await chrome.privacy.websites.referrersEnabled.set(arg);
+    await chrome.privacy.websites.thirdPartyCookiesAllowed.set(arg);
+    await chrome.action.setIcon({ path: value ? "off.png" : "on.png" });
   })
 );
 chrome.management.onEnabled.addListener(info =>
-  info.id == chrome.runtime.id && chrome.privacy.websites.referrersEnabled.get({}, details =>
-    details.value && chrome.action.setIcon({ path: "off.png" })
+  info.id == chrome.runtime.id && chrome.privacy.websites.referrersEnabled.get({}, async details =>
+    details.value && await chrome.action.setIcon({ path: "off.png" })
   )
 );
