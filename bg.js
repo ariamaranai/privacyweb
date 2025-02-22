c6928d0788be4a421a89e613777bfcffa2d3211b
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-02-22 12:04:09 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,13 +1,19 @@
-for (let i = 0; i < 7; ++i)
-  chrome.privacy.websites[[
-    "adMeasurementEnabled",
-    "fledgeEnabled",
-    "hyperlinkAuditingEnabled",
-    "relatedWebsiteSetsEnabled",
-    "topicsEnabled",
-    "referrersEnabled",
-    "thirdPartyCookiesAllowed"
-  ][i]].set({ value: !1 });
+chrome.runtime.onInstalled.addListener(() => {
+  for (let i = 0; i < 7; ++i)
+    chrome.privacy.websites[[
+      "adMeasurementEnabled",
+      "fledgeEnabled",
+      "hyperlinkAuditingEnabled",
+      "relatedWebsiteSetsEnabled",
+      "topicsEnabled",
+      "referrersEnabled",
+      "thirdPartyCookiesAllowed"
+    ][i]].set({ value: !1 });
+    chrome.action.setIcon({ path: "on.png" });
+});
+chrome.privacy.websites.referrersEnabled.get({}, details =>
+  chrome.action.setIcon({ path: details.value ? "off.png" : "on.png" })
+);
 chrome.action.onClicked.addListener(async () => {
   let value = !(await chrome.privacy.websites.referrersEnabled.get({})).value;
   chrome.privacy.websites.referrersEnabled.set({ value }),
