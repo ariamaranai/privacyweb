ed29572f41bb2c0b620f2cd1281998efcaa3341e
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-03-07 09:18:15 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -13,13 +13,13 @@ chrome.runtime.onInstalled.addListener(() => {
     i
   );
 });
-chrome.runtime.onStartup.addListener(async () =>
-  chrome.action.setIcon({ path: (await chrome.privacy.websites.referrersEnabled.get({})).value ? "off.png" : "on.png" })
-);
 chrome.action.onClicked.addListener(async () => {
   let value = !(await chrome.privacy.websites.referrersEnabled.get({})).value;
   chrome.action.setIcon({ path: value ? "off.png" : "on.png" });
   chrome.privacy.websites.referrersEnabled.set(value = { value });
   chrome.privacy.websites.thirdPartyCookiesAllowed.set(value);
 });
+chrome.runtime.onStartup.addListener(async () =>
+  chrome.action.setIcon({ path: (await chrome.privacy.websites.referrersEnabled.get({})).value ? "off.png" : "on.png" })
+);
 chrome.runtime.onStartup.dispatch();
