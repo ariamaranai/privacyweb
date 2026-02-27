e2f565fc87dbf651ff13a40ac09eac81bf2ae336
a <106607480+ariamaranai@users.noreply.github.com>
2026-02-27 20:39:52 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -13,17 +13,21 @@ chrome.runtime.onInstalled.addListener(() => {
     ][--i]].set({ value: !1 }),
     i
   );
+  chrome.storage.local.set({ value: !0 });
 });
-chrome.action.onClicked.addListener(async () => {
-  let value = !(await chrome.privacy.websites.referrersEnabled.get({})).value;
-  chrome.action.setIcon({ path: value ? "off.png" : "on.png" });
-  chrome.privacy.websites.referrersEnabled.set(value = { value });
-  chrome.privacy.websites.thirdPartyCookiesAllowed.set(value);
-});
+chrome.action.onClicked.addListener(() =>
+  chrome.storage.local.get("value", v => {
+    let isEnable = v.value;
+    chrome.action.setIcon({ path: isEnable ? "off.png" : "on.png" }),
+    chrome.privacy.websites.referrersEnabled.set(v),
+    chrome.privacy.websites.thirdPartyCookiesAllowed.set(v),
+    chrome.storage.local.set({ value: !isEnable })
+  })
+);
 {
   let isCalled;
   chrome.runtime.onStartup.addListener(async () =>
-    isCalled ??= (chrome.action.setIcon({ path: (await chrome.privacy.websites.referrersEnabled.get({})).value ? "off.png" : "on.png" }), 0)
+    isCalled ??= (chrome.action.setIcon({ path: (await chrome.storage.local.get("0")).value ? "off.png" : "on.png" }), 0)
   );
 }
 chrome.runtime.onStartup.dispatch();
